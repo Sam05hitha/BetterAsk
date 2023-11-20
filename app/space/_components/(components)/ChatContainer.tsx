e981ef94c1475422d09bf83a01110d12de8710b5
Sanket Abhyankar <148947472+Sanket-Abhtankar-webknot@users.noreply.github.com>
2023-11-21 03:45:41 +0530

    Feature/feedback (#14)
    * feature/create-space form (#5)
    
    * update/sidebar collapsable
    
    * update/sidebar input
    
    * update/sidebar input
    
    * update/lint errors
    
    * update/input landing page
    
    * update/start input api integration
    
    * update/start page dependacy error
    
    * update/api integration
    
    * feature/feadback
    
    * feature/response feedback

--- app/space/_components/(components)/ChatContainer.tsx
+++ app/space/_components/(components)/ChatContainer.tsx
@@ -4,6 +4,7 @@ import React, { useRef, useEffect } from "react";
 import { Message } from "..";
 import style from "../../[spaceId]/space.module.scss";
 import { TConversation } from "@/app/_utils/types";
+import { scrollToBottom } from "@/app/_utils/methods";
 
 interface IChatContainer {
   data: TConversation[];
@@ -13,19 +14,12 @@ export default function ChatContainer({ data }: IChatContainer) {
   const chatContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const scrollToBottom = () => {
-      if (chatContainerRef.current) {
-        chatContainerRef.current.scrollTop =
-          chatContainerRef.current.scrollHeight;
-      }
-    };
-
-    scrollToBottom();
+    scrollToBottom(chatContainerRef);
   }, [data]);
 
   return (
     <section ref={chatContainerRef} className={`${style.space_page_container}`}>
-      {Array.isArray(data)  ? (
+      {Array.isArray(data) ? (
         data?.map((item: any, index: any) => (
           <div key={index} className={style.space_page_chat_q_and_a}>
             <Message data={item} isUser={true} />
