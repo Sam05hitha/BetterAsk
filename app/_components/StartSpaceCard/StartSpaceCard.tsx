87cfdbad9168a5e10b65a0240f34c2241162a2b0
Sanket-Abhtankar-webknot <sanket@webknot.in>
2024-02-07 23:26:59 +0530

    modified for interior designs

--- app/_components/StartSpaceCard/StartSpaceCard.tsx
+++ app/_components/StartSpaceCard/StartSpaceCard.tsx
@@ -2,64 +2,64 @@
 
 import React, { useRef, useEffect, useState } from "react";
 import style from "./start_space_card.module.scss";
-import KeyboardBackspaceRoundedIcon from "@mui/icons-material/KeyboardBackspaceRounded";
-import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
+// import KeyboardBackspaceRoundedIcon from "@mui/icons-material/KeyboardBackspaceRounded";
+// import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
 import { ChatInput } from "@/app/space/_components";
 import { useRouter } from "next/navigation";
 import { getCookie, setCookie } from "@/app/_utils/methods";
 import useSendQuery from "@/app/_hooks/useSendQuery";
 
-const slides = [
-  {
-    id: 0,
-    isLast: true,
-    p1: "The Prevention of Sexual Harassment (POSH) Act in India is in place to create a safer and more supportive environment for everyone at the workplace. If you have any questions or if there's anything specific you'd like to know , please feel free to ask.",
-    p2: "",
-    title: "Welcome to BetterAsk - POSH",
-  },
-  {
-    id: 1,
-    isInput: true,
-    p1: "Your feelings and inquiries are valid, and it's important to ensure that everyone feels secure and respected. If you or someone you know is dealing with a situation related to this, there are mechanisms in place for support and resolution.",
-    p2: "Remember that you are not alone, and seeking information is a positive step toward understanding and empowerment.",
-    title: "",
-  },
-];
+// const slides = [
+//   {
+//     id: 0,
+//     isLast: true,
+//     p1: "The Prevention of Sexual Harassment (POSH) Act in India is in place to create a safer and more supportive environment for everyone at the workplace. If you have any questions or if there's anything specific you'd like to know , please feel free to ask.",
+//     p2: "",
+//     title: "Welcome to BetterAsk - POSH",
+//   },
+//   {
+//     id: 1,
+//     isInput: true,
+//     p1: "Your feelings and inquiries are valid, and it's important to ensure that everyone feels secure and respected. If you or someone you know is dealing with a situation related to this, there are mechanisms in place for support and resolution.",
+//     p2: "Remember that you are not alone, and seeking information is a positive step toward understanding and empowerment.",
+//     title: "",
+//   },
+// ];
 
 interface ISlide {
   slideId: number;
 }
 
 export default function StartSpaceCard() {
-  const [slide, setSlide] = useState<ISlide>({
-    slideId: 0,
-  });
+  // const [slide, setSlide] = useState<ISlide>({
+  //   slideId: 0,
+  // });
   const inputRef = useRef<HTMLInputElement | null>(null);
   const [startChatInputValue, setStartChatInputValue] = useState<string>("");
-  const { response, error, getInputQuery, loadingQuery } = useSendQuery();
+  const { response, getInputQuery, loadingQuery } = useSendQuery();
 
   const router = useRouter();
-  const currentUser = getCookie();
+  // const currentUser = getCookie();
 
-  function handleOnSlideStart() {
-    const currentUser = getCookie();
-    if (currentUser) {
-      //On start if user exists go to chat with id
-      router.push(`/space/${currentUser}`);
-      return;
-    }
-    setSlide((prev) => ({
-      ...prev,
-      slideId: prev.slideId + 1,
-    }));
-  }
+  // function handleOnSlideStart() {
+  //   const currentUser = getCookie();
+  //   if (currentUser) {
+  //     //On start if user exists go to chat with id
+  //     router.push(`/space/${currentUser}`);
+  //     return;
+  //   }
+  //   setSlide((prev) => ({
+  //     ...prev,
+  //     slideId: prev.slideId + 1,
+  //   }));
+  // }
 
-  function handleOnSlideBack() {
-    setSlide((prev) => ({
-      ...prev,
-      slideId: prev.slideId ? prev.slideId - 1 : 0,
-    }));
-  }
+  // function handleOnSlideBack() {
+  //   setSlide((prev) => ({
+  //     ...prev,
+  //     slideId: prev.slideId ? prev.slideId - 1 : 0,
+  //   }));
+  // }
 
   function handleStartChatInput(event: React.ChangeEvent<HTMLInputElement>) {
     const value = event.target.value;
@@ -89,13 +89,8 @@ export default function StartSpaceCard() {
 
   return (
     <div className={style.start_space_container_main}>
-      <div
-        key={slide?.slideId}
-        className={`${style.start_space_card_items_container} ${
-          style[`slide_${slide.slideId}`]
-        }`}
-      >
-        {slides.map((item) => (
+      {/* <div className={style.start_space_card_items_container}> */}
+      {/* {interiorDesigns.map((item) => (
           <div key={item.id} className={style.start_space_card_item}>
             {item.title && <h2 className="font-bold">{item.title}</h2>}
             <p>{item.p1}</p>
@@ -110,7 +105,7 @@ export default function StartSpaceCard() {
                   <ArrowForwardRoundedIcon />
                 </button>
               )}
-              {slide.slideId > 0 && (
+              {true && (
                 <>
                   <button
                     className={style.back_slide_button}
@@ -119,22 +114,22 @@ export default function StartSpaceCard() {
                     <KeyboardBackspaceRoundedIcon />
                     <span className="font-medium">Back</span>
                   </button>
-                  <ChatInput
-                    inputRef={inputRef}
-                    shadow={false}
-                    custom="bg-white"
-                    loading={loadingQuery}
-                    placeHolder="Ask me anything about posh"
-                    handleOnChange={handleStartChatInput}
-                    handleOnSubmit={handleStartChatSubmit}
-                    value={startChatInputValue}
-                  />
                 </>
               )}
             </div>
           </div>
-        ))}
-      </div>
+        ))} */}
+      {/* </div> */}
+      <ChatInput
+        inputRef={inputRef}
+        shadow={false}
+        custom="bg-transparent"
+        loading={loadingQuery}
+        placeHolder="Ask me anything about Interior designs"
+        handleOnChange={handleStartChatInput}
+        handleOnSubmit={handleStartChatSubmit}
+        value={startChatInputValue}
+      />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import style from "./main.module.scss";
 import { Button, StartSpaceCard } from "./_components";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import Hbg from "../public/home-bg.svg";
+// import Hbg from "../public/home-bg.svg";
 
 const topSpaces = [
   { title: "Salary and Finance", to: "space/1" },
@@ -46,12 +46,12 @@ export default function Home() {
           view more
         </Link>
       </div> */}
-      <header className=" absolute top-[20px] left-[20px] font-geo font-extrabold text-3xl">
+      <header  className="absolute top-[20px] left-[20px] font-geo font-extrabold text-3xl text-white">
         BetterAsk
       </header>
       <StartSpaceCard />
       <div className={style.home_bgImage}>
-        <Image src={Hbg} priority alt="icon" />
+        {/* <Image src={Hbg} priority alt="icon" /> */}
       </div>
     </main>
   );
