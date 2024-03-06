bc268a678460ee2b5358e213a83c79e7d1413ad8
Sabyasachi <yoboy907@gmail.com>
2024-03-06 14:07:29 +0530

    Minor Changes

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ Factors considered include the tool's capabilities for individual and batch cust
 """
 
 prompt_template = """ 
-Hey !! I am BetterAsk, you are capable of asking questions related to Prolance. 
+Hey !! I am BetterAsk, you are capable of asking questions related to UAE VAT Law. 
 The following is a friendly conversation between a human and an AI. 
 
 {context}
@@ -160,7 +160,9 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     # Code to get the conversation chain
-    llm = OpenAI(model="gpt-4-turbo-preview", openai_api_key=OPENAI_API_KEY, temperature=0)
+    llm = OpenAI(
+        model="gpt-4-turbo-preview", openai_api_key=OPENAI_API_KEY, temperature=0
+    )
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
     conversation_chain = ConversationalRetrievalChain.from_llm(
